3590a5a64ba9e6bf373bb93320c860f7e4b8af34
MounikaAlloju <mounika.alloju@gmail.com>
2022-02-17 16:07:21 +0530

    UC2 added

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC2 {
+
+	public static void main(String[] args) {
+		
+	
+	int playrole;
+	int dicevalue;
+	
+	dicevalue=(int)Math.floor((Math.random()*10) %6 )+1;
+	System.out.println(dicevalue);
+	
+}
+}
